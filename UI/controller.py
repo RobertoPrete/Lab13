51e84b7c3f3f990594119f78e32c839ad33e9303
RobertoPrete <robertoprete2001@gmail.com>
2025-07-07 15:08:31 +0200

    risolto punto 1a

--- UI/controller.py
+++ UI/controller.py
@@ -7,6 +7,13 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self._year = None
+
+    def fillDDYear(self):
+        years = self._model.getYears()
+        for year in years:
+            self._view._ddAnno.options.append(ft.dropdown.Option(year))
+        self._view.update_page()
 
     def handleDDYearSelection(self, e):
         pass
--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,24 @@
 from database.DB_connect import DBConnect
 
 
-class DAO():
+class DAO:
     def __init__(self):
         pass
+
+    @staticmethod
+    def getAllYears():
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        result = []
+        query = """select year from seasons s """
+        cursor.execute(query)
+        for row in cursor:
+            result.append(row["year"])
+        cursor.close()
+        conn.close()
+        return result
+
+
+if __name__ == "__main__":
+    print(DAO.getAllYears())
+
